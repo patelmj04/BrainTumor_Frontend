6d8a536df5f51faa811b94e55448f91becd59171
patelmj04 <patelmj@rknec.edu>
2024-11-27 22:21:08 +0530

    neuro-2

--- src/components/NavigationPill/NavigationPill.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./NavigationPill.css";
-
-const NavigationPill = ({ text }) => {
-  return <button className="navigationPill">{text}</button>;
-};
-
-export default NavigationPill;
